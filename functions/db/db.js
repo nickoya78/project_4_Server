c43f15ce87b4b0a93891da114591c05c6f11952c
Nicholas Oyagh <you@example.com>
2023-03-22 20:42:02 -0700

    changes made

--- functions/db/db.js
+++ functions/db/db.js
@@ -1,14 +1,5 @@
 const knex = require("knex");
-// import knex from "knex";
 const knexfile = require("../../knexfile");
-// const fetch = require("node-fetch");
 const db = knex(knexfile.development);
 
 module.exports = db;
-
-// exports.handler = async function (event, context) {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(),
-//   };
-// };
